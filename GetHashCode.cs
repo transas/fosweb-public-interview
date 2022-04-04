a809f8200a6c6c005d856bc0b8508b4696c701ee
lukas_sergey <sergey.lukashevich@wartsila.com>
2022-04-04 17:26:18 +0400

    Add GetHashCode

---
+++ GetHashCode.cs
@@ -0,0 +1,22 @@
+﻿namespace Transas.Examples.GetHashCode
+{
+    public class Test1
+    {
+        public int Id { get; set; }
+
+        public override int GetHashCode()
+        {
+            return new Random().Next(1000);
+        }
+    }
+
+    public class Test2
+    {
+        public int Id { get; set; }
+
+        public override int GetHashCode()
+        {
+            return 1000;
+        }
+    }
+}
