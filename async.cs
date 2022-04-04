3224f38519b3a4f4e61dc8ad5f12730aae99a7dc
lukas_sergey <sergey.lukashevich@wartsila.com>
2022-04-04 17:47:55 +0400

    async

---
+++ async.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Transas.Examples.Ioc
+{
+    public interface IDbRepository
+    {
+        Task Save(string text);
+    }
+
+    public class DbRepository : IDbRepository
+    {
+        public async Task Save(string text)
+        {
+            // save text to database
+        }
+    }
+}
